584962305be1c29bd17aa5ab0a3472dd08d43793
Leonardo Machado Britto Junior <leo.dev2001@gmail.com>
2025-04-18 22:46:21 +0000

    A

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ function iniciarPerguntas() {
     
     medoBtn.addEventListener('click', () => {
         if (medoInput.value.trim() === '') {
-            document.body.style.backgroundImage = "url('https://github.com/LeonardoDevBa/medo/blob/main/files/%F0%9D%90%80%F0%9D%90%8B%F0%9D%90%88%F0%9D%90%80%F0%9D%90%92%20%E1%8F%90%20%F0%9D%95%B2%F0%9D%90%AB%F0%9D%90%9A%F0%9D%90%A9%F0%9D%90%A1%F0%9D%90%A2%F0%9D%90%9C%20%F0%9D%95%BB%F0%9D%90%A8%F0%9D%90%AB%F0%9D%90%AD%F0%9D%90%9F%F0%9D%90%A8%F0%9D%90%A5%F0%9D%90%A2%F0%9D%90%A8.gif')";
+            document.body.style.backgroundImage = "url('https://github.com/LeonardoDevBa/medo/blob/main/files/nome.gif')";
             document.body.style.backgroundSize = "cover";
             document.body.style.backgroundRepeat = "no-repeat";
             resp.innerHTML = '<p style="color: red;">Por favor, responda à pergunta.</p>';
         } else {
             const medoUsuario = medoInput.value.charAt(0).toUpperCase() + medoInput.value.slice(1);
-            document.body.style.backgroundImage = "url('files/download (5).gif')";
+            document.body.style.backgroundImage = "url('https://github.com/LeonardoDevBa/medo/blob/main/files/medo.gif')";
             document.body.style.backgroundSize = "cover";
             document.body.style.backgroundRepeat = "no-repeat";
             resp.innerHTML = `<p style="color: gray;">Então ${nome.value}, estamos indo atrás de você junto com seu maior medo, agora eu e o ${medoUsuario} somos amigos, e vamos te pegar!.</p>`;
